0b8c30259dd082c19608ac1d519de08d06ebb119
Vito Vittore <vito.vittore@gmail.com>
2021-05-30 12:13:18 -0500

    Added the files for Chapter 1

---
+++ src/utils/colors.js
@@ -0,0 +1,55 @@
+export const blue = {
+    100: '#3a36e0',
+    200: '#0804b8',
+    300: '#030086',
+    400: '#5f25a4',
+    500: '#050449',
+  };
+  
+  export const green = {
+    100: '#529e66',
+    200: '#367b48',
+    300: '#276738',
+  };
+  
+  export const yellow = {
+    100: '#e1c542',
+    200: '#cab23f',
+    300: '#b49e35',
+  };
+  
+  export const red = {
+    100: '#d0454c',
+    200: '#b54248',
+    300: '#95353a',
+  };
+  
+  export const neutral = {
+    100: '#ffffff',
+    200: '#f4f5f7',
+    300: '#e1e1e1',
+    400: '#737581',
+    500: '#4a4b53',
+    600: '#000000',
+  };
+  
+  export const colors = {
+    100: '#F7E4E5',
+    200: '#BFB0B1',
+    300: '#FA9BA1',
+    400: '#CCB1B3',
+    500: '#E3ACB0',
+    600: '#000000',
+  };
+  
+  export const highlight = {
+    blue: '#0000FF',
+  };
+  
+  export const text = {
+    dark: '#000000',
+    light: '#ffffff',
+    highlight: '#0000FF',
+    color: '#FA9BA1',
+    inverted: '#CCB1B3',
+  };
